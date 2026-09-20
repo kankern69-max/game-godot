db0d445ebf28297978b07eefe010ec4c67e1d327
thijmenb1 <thijmen.ka.bruins@outlook.com>
2026-09-20 16:45:25 +0200

    Expand component system and unlock resources
    This commit broadens the component model with typed component categories and adds new metadata fields for switch, capacitor, diode, resistor, and transistor definitions. It also introduces Battery and Ground component scripts, updates the global unlocked-component list to preload capacitor and diode resources, and refreshes the component tile UI to display name, sprite, and type/cost information.

---
+++ pcd-desinger-v-1/components/resource_scripts/battery.gd
@@ -0,0 +1,5 @@
+extends Component
+class_name Battery
+@export var voltage: float
+@export var internal_resistance: float
+@export var max_current: float
--- pcd-desinger-v-1/singeltons/Global.gd
+++ pcd-desinger-v-1/singeltons/Global.gd
@@ -5,4 +5,6 @@ enum MODE {ARCADE, SIMULATOR}
 
 var current_mode: MODE = MODE.ARCADE
 
-var unlocked_components: Array[Component] = []
+var capacitor = preload("res://components/component_resources/Capacitor_1.tres")
+var diode = preload("res://components/component_resources/Diode_1.tres")
+var unlocked_components: Array[Component] = [capacitor, diode]
