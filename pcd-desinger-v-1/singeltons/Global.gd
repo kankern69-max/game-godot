cc3b2112dcb84a4f1ccb669d851ddfb1aaa1a55f
thijmenb1 <thijmen.ka.bruins@outlook.com>
2026-09-20 20:23:38 +0200

    Fix menu merge conflict
    Fix menu merge conflict

--- pcd-desinger-v-1/singeltons/Global.gd
+++ pcd-desinger-v-1/singeltons/Global.gd
@@ -5,6 +5,4 @@ enum MODE {ARCADE, SIMULATOR}
 
 var current_mode: MODE = MODE.ARCADE
 
-var capacitor = preload("res://components/component_resources/Capacitor_1.tres")
-var diode = preload("res://components/component_resources/Diode_1.tres")
-var unlocked_components: Array[Component] = [capacitor, diode]
+var unlocked_components: Array[Component] = []
