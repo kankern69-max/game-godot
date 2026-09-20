323d6bb44ec7626540091a80b8dad3ad8f156b15
thijmenb1 <thijmen.ka.bruins@outlook.com>
2026-09-20 14:23:30 +0200

    Add unlockable component UI
    This change adds tracked unlocked components to the global state and builds a dynamic component list in the UI. Each component now gets a dedicated tile scene, and the component panel redraws whenever the unlocked list changes so available parts render automatically.

--- pcd-desinger-v-1/singeltons/Global.gd
+++ pcd-desinger-v-1/singeltons/Global.gd
@@ -4,3 +4,5 @@ signal toolChanged(toolName: String)
 enum MODE {ARCADE, SIMULATOR}
 
 var current_mode: MODE = MODE.ARCADE
+
+var unlocked_components: Array[Component] = []
--- pcd-desinger-v-1/ui/component_ui.gd
+++ pcd-desinger-v-1/ui/component_ui.gd
@@ -1,4 +1,18 @@
 extends Panel
 
+var componentTile := preload("res://ui/component_tile.tscn")
+
+@onready var Vbox : VBoxContainer = $components/VBoxContainer
+
 func _process(delta):
-	pass
+	if Global.unlocked_components.size() != Vbox.get_child_count():
+		draw_components()
+	
+func draw_components() -> void:
+	for child in Vbox.get_children():
+		child.queue_free()
+	
+	for component in Global.unlocked_components:
+		var tile := componentTile.instantiate()
+		tile.component = component
+		Vbox.add_child(tile)
